abb0b99932f80460e9f3c5f78e72bd73707dd6ee
is-2218 <infostretch@is-2218s-MacBook-Pro.local>
2017-05-10 16:12:40 -0700

    initial props

---
+++ appProperties.groovy
@@ -0,0 +1,15 @@
+﻿BUILD_TOOL="";
+BUILD_PROP="";
+BUILD_HOME="";
+
+def getBuildTool(){
+    return BUILD_TOOL;
+}
+
+def getBuildProps(){
+    return BUILD_PROP;
+}
+
+def getBuildHome(){
+    return BUILD_TOOL;
+}
