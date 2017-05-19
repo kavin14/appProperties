d2b505e0aa357016f61edb40b01630bcd799de9c
kavin14 <kavin.isaac@gmail.com>
2017-05-19 14:52:14 -0700

    Create postDataToElasticSearch.groovy
    ES

---
+++ vars/postDataToElasticSearch.groovy
@@ -0,0 +1,28 @@
+def call() {
+ TimeZone.getTimeZone('UTC')
+ Date date= new Date()
+ String newdate=date.format("YYYY-MM-DD HH:mm:ss.Ms")
+ def buildType = "Release";
+ def currentStage = "Build";
+ def deployJson = """
+  {
+            "scm_branch": "git",
+            "job_name": "$env.JOB_NAME",
+            "job_url": "$env.JOB_URL",
+            "build_url": "$env.BUILD_URL",
+            "environment": "Test",
+            "application_name": "$env.JOB_NAME",
+            "artifact": "",
+            "deploy_url": "$env.BUILD_URL",
+            "result": "${currentBuild.result}",
+            "@timestamp": "$newdate",
+            "build_type" : "$buildType",
+            "stage": "$currentStage"
+        }
+"""
+["curl", "-i", "-XPOST", 
+         "-H 'Content-Type:application/json'", 
+         "-d ${deployJson}", 
+         "http://10.20.28.90:9201/wf-build-data/jobdata/"].execute().text
+
+}
