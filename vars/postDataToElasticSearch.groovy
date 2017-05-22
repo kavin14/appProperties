4bf9b6a254ac4e895a5dabc7a157b0ab930e70df
kavin14 <kavin.isaac@gmail.com>
2017-05-22 15:36:38 -0700

    Update postDataToElasticSearch.groovy

--- vars/postDataToElasticSearch.groovy
+++ vars/postDataToElasticSearch.groovy
@@ -1,4 +1,16 @@
+#!/usr/bin/env groovy
+import hudson.model.*
+import hudson.EnvVars
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import java.net.URL
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+ 
 def call(build_Type,current_Stage) {
+
  TimeZone.getTimeZone('UTC')
  Date date= new Date()
  String newdate=date.format("YYYY-MM-DD HH:mm:ss.Ms")
@@ -24,4 +36,21 @@ def call(build_Type,current_Stage) {
          "-H 'Content-Type:application/json'", 
          "-d ${deployJson}", 
          "http://10.20.28.90:9201/wf-build-data/jobdata/"].execute().text
+ 
+ http.request( 'http://admin:admin@10.20.28.90:8081', GET, TEXT ) { req ->
+  uri.path = '/jenkins-team-1/job/maven/21/api/json?tree=number,status,timestamp,id,result'
+  headers.'User-Agent' = "Mozilla/5.0 Firefox/3.0.4"
+  headers.Accept = 'application/json'
+
+  response.success = { resp, reader ->
+    assert resp.statusLine.statusCode == 200
+    println "Got response: ${resp.statusLine}"
+    println "Content-Type: ${resp.headers.'Content-Type'}"
+    println reader.text
+  }
+
+  response.'404' = {
+    println 'Not found'
+  }
+}
 }
