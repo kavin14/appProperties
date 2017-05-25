284aaa6116e9793d252970b52e7d0eefe09d0762
kavin14 <kavin.isaac@gmail.com>
2017-05-25 16:04:19 -0700

    Update postStageData.groovy

--- vars/postStageData.groovy
+++ vars/postStageData.groovy
@@ -8,18 +8,24 @@ import groovy.json.JsonSlurper
 
 def call(build_Type){
      def x = "$env.BUILD_URL".split('/')
+     def jobendtime = new Date()
      curlCommand = 'curl  -H "Accept: application/json" -H "Content-Type: application/json" -u admin:admin '
      buildUrlQuery = "wfapi/"
      buildDetails = "$curlCommand$env.BUILD_URL$buildUrlQuery".execute().text
      def jsonSlurper = new JsonSlurper()
      def object = jsonSlurper.parseText(buildDetails)
      assert object instanceof Map
-     object << [buildType: 'Release']
+     object << [buildType: build_Type]
      //object << [masterName: x[3].replace('-','')]
      object << [masterName: x[3]]
      object << [JobName: x[5]]
+     object << [@timestamp: jobendtime]
+      if(currentBuild.result != "FAILIURE" || currentBuild.result != "ABORTED") {
+         currentbuild.result = "SUCCESS"
+     }
+     object << [buildStatus: currentbuild.result]
      buildDetails = JsonOutput.toJson(object)
-
+    
 ["curl", "-i", "-XPOST", 
          "-H 'Content-Type:application/json'", 
          "-d ${buildDetails}", 
