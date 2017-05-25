3a45e4b7abc5f008e7d3dc7a8d850a8dc7cf0608
kavin14 <kavin.isaac@gmail.com>
2017-05-25 12:39:41 -0700

    Update postStageData.groovy

--- vars/postStageData.groovy
+++ vars/postStageData.groovy
@@ -7,6 +7,7 @@ import java.net.URL
 import groovy.json.JsonSlurper
 
 def call(build_Type){
+     def x = "http://10.20.28.90:8081/jenkins-team-1/job/maven/".split('/')
      curlCommand = 'curl  -H "Accept: application/json" -H "Content-Type: application/json" -u admin:admin '
      buildUrlQuery = "wfapi/"
      buildDetails = "$curlCommand$env.BUILD_URL$buildUrlQuery".execute().text
@@ -14,6 +15,8 @@ def call(build_Type){
      def object = jsonSlurper.parseText(buildDetails)
      assert object instanceof Map
      object << [buildType: build_Type]
+     object << [masterName: x[3]]
+     object << [JobName: x[5]]
      buildDetails = JsonOutput.toJson(object)
 
 ["curl", "-i", "-XPOST", 
